fcf7be31d6f6a60b8fca99bb3b152fc305f75e14
DzmitrySm <magicjoker@yandex.ru>
2022-04-30 19:04:58 +0300

    feat: start task

--- src/stack.js
+++ src/stack.js
@@ -13,9 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(data) {
+    this._size = 0;
+    this._storage = {};
   }
 
   pop() {
