993f3c27c41c16d69ba4d12147d2fa2ffcffdbdb
DzmitrySm <magicjoker@yandex.ru>
2022-05-02 01:04:28 +0300

    feat: add linked list task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,20 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (Node.root === null)
+      this.root = null 
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    
   }
 
   has(/* data */) {
@@ -42,7 +41,9 @@ class BinarySearchTree {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
+
 }
+
 
 module.exports = {
   BinarySearchTree
